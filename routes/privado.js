952d6eb1a942dfd2a91879a04c9387ad9835c6fc
HLeonardoAfonso <162568940+HLeonardoAfonso@users.noreply.github.com>
2024-04-26 09:18:10 +0100

    add html root

--- routes/privado.js
+++ routes/privado.js
@@ -4,7 +4,7 @@ const privadoRouter = require('express').Router();
 // Define uma rota para a página HTML
 privadoRouter.get('/gerirCarros', (req, res) => {
     // Envie o arquivo HTML como resposta para a solicitação HTTP
-    res.sendFile('/Users/ninatchernega/Desktop/Dada-p2/templates/backOffice/tabelaCarros.html');
+    res.sendFile('templates/backOffice/artistPage.html', { root: "." });
   });
 
 
--- routes/publico.js
+++ routes/publico.js
@@ -4,7 +4,7 @@ const publicoRouter = require('express').Router();
 // Define uma rota para a página HTML
 publicoRouter.get('/', (req, res) => {
     // Envie o arquivo HTML como resposta para a solicitação HTTP
-    res.sendFile('/Users/ninatchernega/Desktop/Dada-p2/templates/frontEnd/index.html');
+    res.sendFile('templates/frontEnd/index.html', { root: "." });
   });
 
 
