6590ef8ad309d37baccfe2a049ecb4abb204f67b
HLeonardoAfonso <162568940+HLeonardoAfonso@users.noreply.github.com>
2024-05-11 18:18:49 +0100

    uhhhh edit works

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,91 +1,117 @@
 const fs = require('fs');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient()
+
+//TEST
+exports.testConnection = async (req, res) => {
+    try {
+        await prisma.$connect();
+        res.send('success');
+    } catch (error) {
+        res.send('error with sql', error);
+    } finally {
+        await prisma.$disconnect();
+    }
+}
 
 //returns all art listings
 exports.getAll = async (req, res) => {
-    //read file
-    const datajson = fs.readFileSync('data/local/data.json', "utf-8");
-    //parse json
-    const data = JSON.parse(datajson);
-    //return listings
-    return res.send(data.art);
+    try {
+        //le toda a tabela
+        const response = await prisma.Listing.findMany();
+        res.status(200).json(response)
+    } catch (error) {
+        res.status(500).json({ msg: error.message })
+    }
 }
 
 //returns art listing based on given id
 exports.getById = async (req, res) => {
-    //get listing id
-    const id = req.params.id;
-    //read file
-    const datajson = fs.readFileSync("data/local/data.json", "utf-8");
-    //parse json
-    const data = JSON.parse(datajson);
-    //search listing with the id
-    const art = data.art.filter(art => art.id == id);
-    //return listing
-    res.send(art);
+    const id = req.params.id*1;
+    try {
+        //procura o carro com o id
+        const response = await prisma.Listing.findUnique({
+            where: {
+                id: id,
+            },
+        })
+        //devolve o carro
+        res.status(200).json(response)
+    } catch (error) {
+        res.status(404).json({ msg: error.message })
+    }
 }
 
 
 //create new listing
 exports.create = async (req, res) => {
-    //obtain data from form
-    const {id, title, price, year, tag, colection, description, rating} = req.body;
-    //read file 
-    const datajson = fs.readFileSync("data/local/data.json", "utf-8");
-    //parse json
-    const data = JSON.parse(datajson);
-    //add new listing to the list
-    data.art.push(req.body);
-    //create a new file with the new listing
-    fs.writeFileSync('data/local/data.json', JSON.stringify(data));
-    //return
-    return res.status(201).send(req.body);
+    const { title, price, year, tags, colection, description, coverimg, rating } = req.body;
+    try {
+        //criar um novo carro
+        const art = await prisma.Listing.create({
+            data: {
+                title: title,
+                price: price,
+                year: year,
+                tags: tags,
+                colection: colection,
+                description: description,
+                coverimg: coverimg,
+                rating: rating
+            },
+        })
+        //devolve o carro criado
+        res.status(201).json(art)
+    } catch (error) {
+        res.status(400).json({ msg: error.message })
+    }
 }
 
 //update listing
 exports.update = async (req, res) => {
-    //get the listing by id
-    const {id, title, price, year, tag, colection, description} = req.body;
-    //read file
-    const datajson = fs.readFileSync("data/local/data.json", "utf-8");
-    //parse json
-    const data = JSON.parse(datajson);
-    //search the listing with the id
-    const art = data.art.find(art => art.id == id);
-    //update listing information
-    art.title = title;
-    art.price = price;
-    art.year = year;
-    art.tag = tag;
-    art.colection = colection;
-    art.description = description;
-    //create a new file with the new data
-    fs.writeFileSync('data/local/data.json', JSON.stringify(data));
-    //return
-    return res.send({id, title, price, year, tag, colection, description});
+    const { title, price, year, tags, colection, description, coverimg, rating } = req.body;
+    try {
+        //procurar o carro com id e atualizar os dados
+        const art = await prisma.Listing.update({
+            where: {
+                id: id*1,
+            },
+            data: {
+                title: title,
+                price: price,
+                year: year,
+                tags: tags,
+                colection: colection,
+                description: description,
+                coverimg: coverimg,
+                rating: rating
+            },
+        })
+        //devolve o carro atualizado
+        res.status(200).json(art)
+    } catch (error) {
+        res.status(400).json({ msg: error.message })
+    }
+
 }
 
 
 //delete listing by id
 exports.delete = async (req, res) => {
-    //get the listing by id
+    //le o id do carro
     const id = req.params.id;
-    //read file
-    const datajson = fs.readFileSync("data/local/data.json", "utf-8");
-    //parse json
-    const data = JSON.parse(datajson);
-    //search index of the listing
-    const artIndex  = data.art.findIndex(art => art.id == id);
-    //verify that the listing was found
-    if (artIndex !== -1) {
-        //remove the listing
-        const deleteListing = data.art.splice(artIndex, 1)[0];
-        //create a new file with the new data
-        fs.writeFileSync('data/local/data.json', JSON.stringify(data));
-        //return
-        return res.status(200).send(deleteListing);
-    } else {
-        //if not found, return error
-        return res.status(404).send("Art work not found");
+    try {
+        //delete student
+        await prisma.Listing.delete({
+            where: {
+                id: id*1,
+            },
+        })
+        //just return ok
+        res.status(200).send("ok");
+    } catch (error) {
+        res.status(400).json({ msg: error.message })
     }
+
 }
 
--- routes/listings.js
+++ routes/listings.js
@@ -1,11 +1,15 @@
 const artRouter = require('express').Router();
 const controller = require('../controllers/listings');
 
+
+artRouter.get('/testCon', controller.testConnection);
 //CRUD for the listings
 artRouter.get('/', controller.getAll);
 artRouter.get('/:id', controller.getById);
 artRouter.post('/create', controller.create);
 artRouter.put('/update', controller.update);
 artRouter.delete('/delete/:id', controller.delete);
+
+
 
 module.exports = artRouter;
