98295fdc5dc2cd1744f42025681aa6d5ef5e782a
HLeonardoAfonso <162568940+HLeonardoAfonso@users.noreply.github.com>
2024-05-23 12:39:22 +0100

    fff

--- server.js
+++ server.js
@@ -5,25 +5,16 @@ const cors = require('cors');
 const express = require('express');
 const multer = require('multer');
 
-
 const storage = multer.diskStorage({
-  destination: 'files/upload/', 
-  filename: (req , file , callback) => {
-    return callback (null, `${Date.now()}${path.extname(file.originalname)}`)
-  }
+    destination: 'files/upload/', 
+    filename: (req , file , callback) => {
+        return callback (null, `${Date.now()}${path.extname(file.originalname)}`)
+    }
 })
 
-const upload = multer ({
-    storage : storage,
-
-})
-
+const upload = multer ({ storage : storage })
 
 const path = require('path');
-
-// const upload = multer({ dest: 'files/upload/'});
-
-
 
 const homepageRouter = require('./routes');
 const router = require('./routes/api');
@@ -34,24 +25,25 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+// Static inports
+
 app.use(express.static('templates/index.html'));
 
 app.use(express.static('styles')); //inport styles
 app.use(express.static('assets')); //inport assets
-app.use(express.static('files/uploads/')); //inport uploaded fotos 
 app.use(express.static('scripts')); //inport assets
+
+app.use(express.static('files/uploads/')); //inport uploaded fotos
+
+// URL routing
 
 app.use('/', homepageRouter);   //homepage
 app.use('/api/', router);       //editing listings api
 app.use('/artist/', privateRouter);  //artist private page
 
-
-
 app.post('/upload', upload.single('file'), (req, res) => {
     res.json(req.file);
 });
-
-
 
 const port = process.env.SERVER_PORT || 8080;
 app.listen(port, () => {
